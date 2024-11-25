0b92db6889b32a1c5eb114c97bb1e7a7374bcd8b
liwangivone <liwangivone@gmail.com>
2024-11-25 12:54:56 +0800

    migration for cds

--- CRUD-LMS/database/migrations/2024_11_25_045147_c_ds.php
+++ CRUD-LMS/database/migrations/2024_11_25_045147_c_ds.php
@@ -11,7 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('c_d_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->text('description');
+            $table->bigInteger('price')->default(0);
+            $table->integer('stock')->default(0);
+            $table->date('datePublished');
+            $table->enum('genre', ['fiction', 'nonfiction', 'fantasy', 'mystery', 'science_fiction', 'biography']);
+            $table->text('onlineLink');
+            $table->string('catalogue_type')->default('CD');
+        });
     }
 
     /**
@@ -19,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('c_d_s');
     }
 };
